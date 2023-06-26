2a6b491dc9d644353ea008393e954a9a0def82e4
roxanastoian <131897578+roxanastoian@users.noreply.github.com>
2023-06-26 21:59:17 +0100

    Added clock feature
    Only shows current time

--- javascript/index.js
+++ javascript/index.js
@@ -68,3 +68,25 @@ setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
+
+setInterval(setClock, 1000);
+
+let hourHand = document.querySelector("[data-hour-hand]");
+let minuteHand = document.querySelector("[data-minute-hand]");
+let secondHand = document.querySelector("[data-second-hand]");
+
+function setClock() {
+  let currentDate = new Date();
+  let secondsRatio = currentDate.getSeconds() / 60;
+  let minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  let hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
